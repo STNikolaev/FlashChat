d7a5aefbd655e94980b3f14707a4571680e3a953
STNikolaev <a.o.nikolaev@icloud.com>
2022-03-03 18:34:36 +0300

    Ready

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Alexandr Nikolaev on 23.02.22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,29 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Alexandr Nikolaev on 23.02.22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
